5767c6acb4595140643659c25f079c239634a5dc
Dorian Gruselin <dorian.gruselin@hotmail.com>
2020-12-07 03:45:44 +0100

    Fix: edge case id citoyen modifié par l'utilisateur

--- src/utils/api.js
+++ src/utils/api.js
@@ -8,53 +8,65 @@ const axios = Axios.create({
     baseURL: process.env.VUE_APP_API_URL,
 });
 
-export default {
-    sendQrCode: async (content, date) => {
-        let id_qr_code, type_createur;
-        try {
-            ({ id_qr_code, type_createur } = JSON.parse(content));
-        } catch(err) {
+async function sendQrCode(content, date) {
+    let id_qr_code, type_createur;
+    try {
+        ({ id_qr_code, type_createur } = JSON.parse(content));
+    } catch(err) {
+        throw new InvalidQrCodeError();
+    }
+
+    try {
+        await axios.post("/citoyens/qr-code", null, {
+            params: {
+                id_citoyen: localStorage.getItem('uuid'),
+                id_qr_code,
+                type_createur,
+                date_entree: date || new Date().toISOString(),
+            },
+        });
+    } catch(err) {
+        if(err.response.status === 422) {
             throw new InvalidQrCodeError();
         }
+        throw new HttpError(err.response.message || err.message, err.response.status);
+    }
+}
 
-        try {
-            const response = await axios.post("/citoyens/qr-code", null, {
-                params: {
-                    id_citoyen: localStorage.getItem('uuid'),
-                    id_qr_code,
-                    type_createur,
-                    date_entree: date || new Date().toISOString(),
-                },
-                validateStatus: status => status < 400 || status === 422,
-            });
-            if(response.status === 422) {
-                throw new InvalidQrCodeError();
-            }
-        } catch(err) {
-            throw new HttpError(err.response.message, err.response.status);
-        }
-        
-    },
-    register: async (token) => {
-        const { data } = await axios.post('/citoyens/enregistrement', {
+async function register(token) {
+    try {
+        const response = await axios.post('/citoyens/enregistrement', {
             token_fcm: token,
         });
-        if(data?.status !== 200) {
-            throw new HttpError(data.message, data.status);
-        }
         await persistStorage();
-        localStorage.setItem('uuid', data.id_citoyen);
-    },
-    updateToken: async (token, uuid) => {
-        const { data } = await axios.put('/citoyens/mise-a-jour', {
+        localStorage.setItem('uuid', response.id_citoyen);
+    } catch(err) {
+        throw new HttpError(err.response.message || err.message, err.response.status);
+    }
+}
+
+async function updateToken(token, uuid) {
+    try {
+        await axios.put('/citoyens/mise-a-jour', {
             token_fcm: token,
         }, {
             params: {
                 id_citoyen: uuid,
             },
         });
-        if(data?.status !== 200) {
-            throw new HttpError(data.message, data.status);
+    } catch(err) {
+        if(err.response.status === 422) {
+            // L'ID citoyen a été altéré par l'utilisateur, on n'a pas d'autre choix que
+            // d'en demander un autre en passant la nouvelle token
+            register(token);
+            throw new Error('ID citoyen invalide');
         }
+        throw new HttpError(err.response.message || err.message, err.response.status);
     }
+}
+
+export default {
+    sendQrCode,
+    register,
+    updateToken,
 }
